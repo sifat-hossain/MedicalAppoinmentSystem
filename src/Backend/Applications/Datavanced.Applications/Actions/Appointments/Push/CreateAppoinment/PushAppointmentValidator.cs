e923910bb6881ac192f858d9444e0891376b7249
md sifat hossain <sifat1258@gmail.com>
2025-09-26 23:17:31 +0600

    create appointment validator

---
+++ src/Backend/Applications/Datavanced.Applications/Actions/Appointments/Push/CreateAppoinment/PushAppointmentValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+
+namespace Datavanced.Applications.Actions.Appointments.Push.CreateAppoinment;
+
+public class PushAppointmentValidator : AbstractValidator<PushAppointmentCommand>
+{
+    public PushAppointmentValidator()
+    {
+        RuleFor(appointment => appointment.AppoitmentDate)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Appoitment Date can not be null or empty");
+
+        RuleFor(appointment => appointment.PatientId)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Patient information can not be null or empty");
+
+        RuleFor(appointment => appointment.DoctorId)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Doctor information can not be null or empty");
+
+        RuleFor(appointment => appointment.Diagnosis)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Diagnosis can not be null or empty");
+    }
+}
